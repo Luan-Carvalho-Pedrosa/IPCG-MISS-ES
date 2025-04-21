ca48de5ecb6c2eff02999f0cb15f19f65b003bc3
Luan Pedrosa <luanpedrosaprofessional@gmail.com>
2025-04-21 10:39:54 -0300

    implementacao de abertura de tela

--- src/main/java/tzin_ltda/ipcg_missoes/IpcgMissoesApplication.java
+++ src/main/java/tzin_ltda/ipcg_missoes/IpcgMissoesApplication.java
@@ -1,13 +1,38 @@
 package tzin_ltda.ipcg_missoes;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class IpcgMissoesApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(IpcgMissoesApplication.class, args);
+	}
+
+	@Bean
+	public CommandLineRunner commandLineRunner(@Value("${server.port}") String port, 
+											@Value("${abrir.navegador:true}") boolean abrirNavegador) {
+		return args -> {
+			if (abrirNavegador) {
+				String url = "http://localhost:" + port;
+				String os = System.getProperty("os.name").toLowerCase();
+				String command;
+
+				if (os.contains("win")) {
+					command = "cmd /c start firefox " + url;
+				} else if (os.contains("mac")) {
+					command = "open -a Firefox " + url;
+				} else {
+					command = "firefox " + url;
+				}
+
+				Runtime.getRuntime().exec(command);
+			}
+		};
 	}
 
 }
